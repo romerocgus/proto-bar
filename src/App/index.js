38b8ba073d4f01bf6f683e7077e41d80fbc32b84
Gustavo Romero Cañas <romeroc.gus@gmail.com>
2021-11-28 17:05:42 +0100

    done first view

--- src/App/index.js
+++ src/App/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Footer } from "../Components/Footer";
 import { Outlet } from "react-router-dom";
-// import './App.css';
+import './App.css';
 
 function App() {
   return (
--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -1,11 +1,46 @@
-import { Link } from "react-router-dom";
+import React from "react";
+import { Category } from "../../Components/Category";
+import brandLogo from "../../Images/homeBurger.svg";
+import './Home.css';
+
+import entrantesImg from "../../Images/fries.jpg";
+import burgersImg from "../../Images/burger.jpg";
+import veggiesImg from "../../Images/veggie.jpg";
+import drinksImg from "../../Images/beer.jpg"
+const categories = [
+    {
+        name:"entrantes",
+        img: entrantesImg,
+    },
+    {
+        name:"burgers",
+        img: burgersImg,
+    },
+    {
+        name:"veggies",
+        img: veggiesImg,
+    },
+    {
+        name:"bebidas",
+        img: drinksImg,
+    },
+];
+
 function Home(){
     return (
-        <div>
-            <h1>ESTE ES EL HOME</h1>
-            <Link to="/entrantes">Entrantes</Link>
-        </div>
-        
+        <React.Fragment>
+            <header className="header">
+                <img src={brandLogo} alt=""/>
+                <h1 className="header__brand">BRAND</h1>
+            </header>
+            <main>
+                <h2 class= "about">"las mejores hamburguesas de tu vida"</h2>
+                {categories.map(cat =>
+                    <Category name={cat.name} img={cat.img}/>
+                )
+                }
+            </main>
+        </React.Fragment>
 
     );
 }
