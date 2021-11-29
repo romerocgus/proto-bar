78d76ef9f9f0a2956e4c4b1722f2abe8d54fa939
Gustavo Romero Cañas <romeroc.gus@gmail.com>
2021-11-29 03:04:02 +0100

    done navbar component

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,18 +1,29 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import brandLogo from "../../Images/homeBurger.svg";
 import './Navbar.css';
 
 function Navbar(){
+    const [listVisible, setListVisible] = React.useState(false);
+
+    const toggleList = ()=>{
+        setListVisible(!listVisible);
+    }
     return(
         <nav className="navbar">
-            <img className="navbar__img" src={brandLogo} alt= ""/>
-            <span className="navbar__brand">BRAND</span>
-            <div className="navbar__button"></div>
-            <ul className="navbar__list">
-                <li className="navbar__item">ENTRANTES</li>
-                <li className="navbar__item">BURGERS</li>
-                <li className="navbar__item">VEGGIES</li>
-                <li className="navbar__item">BEBIDAS</li>
+            <NavLink className="navbar__brand-link"to="/">
+            <div className="navbar__brand">
+                <img className="navbar__brand-img" src={brandLogo} alt= ""/>
+                <span className="navbar__brand-text">BRAND</span>
+            </div>
+            </NavLink>
+            
+            <div className={`navbar__button ${listVisible ? "cross" : ""}`} onClick={toggleList}></div>
+            <ul className={`navbar__list ${listVisible ? "active" : ""}`}>
+                <li className="navbar__item"><NavLink className="navbar__item-link" to="/entrantes">ENTRANTES</NavLink></li>
+                <li className="navbar__item"><NavLink className="navbar__item-link" to="/burgers">BURGERS</NavLink></li>
+                <li className="navbar__item"><NavLink className="navbar__item-link" to="/veggies">VEGGIES</NavLink></li>
+                <li className="navbar__item"><NavLink className="navbar__item-link" to="/bebidas">BEBIDAS</NavLink></li>
             </ul>
         </nav>
     );
